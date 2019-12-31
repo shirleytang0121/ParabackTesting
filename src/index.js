ed2cc9839864dbf6301725b24fe801edf6c8bd23
yuemintang <shirleytang1996@gmail.com>
2019-12-31 16:35:01 -0500

    change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import createApp from './app';
 
 const app = createApp();
-
+// for testing
 app.listen(process.env.PORT || 8081, () => {
   // eslint-disable-next-line no-console
   console.log('Running on port 8081');
